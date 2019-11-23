e7c2df66d06793d22804a65921ce004b7d6cb211
Ellen Jana Fawkesová <ellenfawkes@gmail.com>
2019-11-23 13:01:16 +0100

    enhanced nightbuild version preparer

--- NpcAdventure/preparenightbuild.js
+++ NpcAdventure/preparenightbuild.js
@@ -1,21 +1,32 @@
-﻿require('child_process').exec('git describe --tags', function (err, stdout) {
+﻿require('child_process').exec("git describe --tags --long HEAD", function (err, stdout) {
     let [tag, commits, hash] = stdout.split('-');
-    let version = /[0-9]+\.[0-9]+\.[0-9]+/.exec(tag)[0];
 
     const fs = require('fs');
 
-    let rawdata = fs.readFileSync('manifest.json');
+    let rawdata = fs.readFileSync('manifest.origin.json');
     let manifest = JSON.parse(rawdata.toString().trim());
-    let date = new Date()
-    let stamp = `${date.getFullYear()}${date.getMonth()}${date.getDate()}`;
-    let newVersion = `${version}-nightbuild.${commits || 0}.${hash || stamp}`;
+    let oldVersion = manifest['Version'];
+    let [base, suffix] = oldVersion.split('-');
+    let [major, minor] = base.split('.');
 
-    console.log("Current version in manifest:", manifest['Version'])
-    console.log("Update to version:", newVersion);
+    if (!suffix) {
+        ++minor;
+    }
 
-    manifest['Version'] = newVersion;
+    let newVersion = `${major}.${minor}.0-${suffix || 'nightbuild'}.${commits || 0}.${hash}`;
+
+    console.log("Version in origin manifest:", oldVersion);
+    console.log("Version described by git:", stdout);
+    console.log("Possible nightbuild version:", newVersion);
+
+    if (commits > 0) {
+        console.log(`Head is ${commits} commit(s) ahead of the last tag ${tag}.`);
+        console.log("Updating version to nightbuild...");
+        manifest['Version'] = newVersion;
+    } else {
+        console.log(`No update version. Head is a last tag ${tag}`);
+    }
 
     fs.writeFileSync('manifest.json', JSON.stringify(manifest, null, 2));
-
-    console.log("Done! Don't forget 'git checkout HEAD manifest.json' after you published nightbuild and continue development and commiting!");
+    console.log("Done!");
 });
