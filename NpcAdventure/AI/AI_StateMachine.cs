23328aace418d10f3dda2dc45f30c1da15f6c8ed
Ellen Jana Fawkesová <ellenfawkes@gmail.com>
2019-11-28 20:38:06 +0100

    Vanilla game nullpointer when try to fight while warping fix

--- NpcAdventure/AI/AI_StateMachine.cs
+++ NpcAdventure/AI/AI_StateMachine.cs
@@ -155,6 +155,8 @@ namespace NpcAdventure.AI
             // Warp NPC to player's location at theirs position
             Helper.WarpTo(this.npc, l, this.player.getTileLocationPoint());
 
+            this.changeStateCooldown = 100;
+
             // Fire location changed event
             this.OnLocationChanged(previousLocation, this.npc.currentLocation);
         }
