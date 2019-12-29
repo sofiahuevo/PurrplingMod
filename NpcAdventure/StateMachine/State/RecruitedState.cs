b70f2c2fa2c1f502fae8a86e1f38650ddd38e869
Ellen Jana Fawkesová <4710267+purrplingcat@users.noreply.github.com>
2019-12-29 20:22:58 +0100

    Added Maru, Emily and Shane location dialogue lines (#69)
    - Added Maru location dialogue lines
    - Added support for only once companion dialogues per game
    - Added ~ for mark randomized dialogues ($ still works, but deprecated)
    - Added Shane and Emily lines
    - French and Portuguese translations for Maru

--- NpcAdventure/StateMachine/State/RecruitedState.cs
+++ NpcAdventure/StateMachine/State/RecruitedState.cs
@@ -15,6 +15,7 @@ using StardewModdingAPI;
 using NpcAdventure.AI;
 using Microsoft.Xna.Framework.Graphics;
 using NpcAdventure.Events;
+using NpcAdventure.Internal;
 
 namespace NpcAdventure.StateMachine.State
 {
@@ -188,11 +189,25 @@ namespace NpcAdventure.StateMachine.State
             this.TryPushLocationDialogue(e.NewLocation);
         }
 
+        private Dialogue GenerateLocationDialogue(GameLocation location, NPC companion)
+        {
+            if (DialogueHelper.GenerateStaticDialogue(companion, location, "companionOnce") is CompanionDialogue dialogueOnce
+                && !this.StateMachine.CompanionManager.Farmer.hasOrWillReceiveMail(dialogueOnce.Tag))
+            {
+                // Remember only once spoken dialogue
+                dialogueOnce.Remember = true;
+                return dialogueOnce;
+            }
+
+            // Generate standard companion various dialogue if no once dialogue defined or once dialogue spoken
+            return DialogueHelper.GenerateDialogue(companion, location, "companion");
+        }
+
         private bool TryPushLocationDialogue(GameLocation location)
         {
             NPC companion = this.StateMachine.Companion;
-            Dialogue newDialogue = DialogueHelper.GenerateDialogue(companion, location, "companion");
             Stack<Dialogue> temp = new Stack<Dialogue>(this.StateMachine.Companion.CurrentDialogue.Count);
+            Dialogue newDialogue = this.GenerateLocationDialogue(location, companion);
 
             if ((newDialogue == null && this.currentLocationDialogue == null) || (newDialogue != null && newDialogue.Equals(this.currentLocationDialogue)))
                 return false;
