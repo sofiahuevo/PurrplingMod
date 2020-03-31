4ef12850cb0c46c3875fa61ecf83167d9c0e6edf
PurrplingCat <ellenfawkes@gmail.com>
2020-03-31 22:40:37 +0200

    updated locale analyzer

--- tools/localeanalyzer.js
+++ tools/localeanalyzer.js
@@ -1,9 +1,12 @@
 const assetsDir = __dirname + "/../NpcAdventure/assets"
+const unofficialLocaleDir = __dirname + "/../unofficial/locale";
 const suffix = "json";
 const supportedLocales = [
-  "pt-BR",
-  "fr-FR",
-  "zh-CN",
+  {code: "pt-BR", name: "Portuguese", dir: assetsDir, official: true},
+  {code: "fr-FR", name: "French", dir: assetsDir, official: true},
+  {code: "zh-CN", name: "Chinese", dir: assetsDir, official: true},
+  {code: "ja-JP", name: "Japanese", dir: unofficialLocaleDir + "/ja-JP", official: false},
+  {code: "ru-RU", name: "Russian", dir: unofficialLocaleDir + "/ru-RU", official: false},
 ];
 const knownContents = [
   "Data/Events",
@@ -40,19 +43,19 @@ function seekForMissing(locale, asset) {
   const fs = require("fs");
   const path = require("path");
   const json = readOriginal(asset);
-  const localizedFilePath = path.resolve(assetsDir, asset + `.${locale}.json`);
+  const localizedFilePath = path.resolve(locale.dir, asset + `.${locale.code}.json`);
 
   if (!fs.existsSync(localizedFilePath)) {
-    console.log(`W01 (untranslated-asset): Asset '${asset}' is not translated into '${locale}'`);
+    console.log(`W01 (untranslated-asset): Asset '${asset}' is not translated into '${locale.code}'`);
     return missing.concat(Object.keys(json) || []);
   }
 
-  const localizedFile = fs.readFileSync(path.resolve(assetsDir, asset + `.${locale}.json`));
+  const localizedFile = fs.readFileSync(localizedFilePath);
   const localizedJson = JSON.parse(localizedFile.toString().trim());
 
   for (let key of Object.keys(json)) {
     if (!Object.keys(localizedJson).includes(key)) {
-      console.log(`W02 (untranslated-key): Missing '${locale}' localization for key '${key}' in asset '${asset}'`);
+      console.log(`W02 (untranslated-key): Missing '${locale.code}' localization for key '${key}' in asset '${asset}'`);
       missing.push(key);
     }
   }
@@ -76,7 +79,11 @@ function walk(locale, contents) {
       const missing = seekForMissing(locale, asset);
       report.push({asset, missing, coverage: coverage(asset, missing.length)})
     } catch (error) {
-      report.push({asset, missing: [], error: error.message})
+      const json = readOriginal(asset);
+      const missing = Object.keys(json) || [];
+
+      report.push({asset, missing: [], error: error.message, coverage: coverage(asset, missing.length)});
+      console.log(`E01 (general-error): An error occured while analysing locale '${locale.code}' asset '${asset}'`);
     }
   }
 
@@ -87,11 +94,16 @@ function generateStats(fullReport) {
   const stats = [];
 
   for (let report of fullReport.reports) {
-    const total = report.analysis.reduce((ac, cur) => ac + cur.coverage.total, 0);
-    const covered = report.analysis.reduce((ac, cur) => ac + cur.coverage.covered, 0)
+    //console.log(report.analysis);
+
+    const total = report.analysis.reduce((ac, cur) => ac + cur.coverage.total || 0, 0);
+    const covered = report.analysis.reduce((ac, cur) => ac + cur.coverage.covered || 0, 0)
 
     stats.push({
       locale: report.locale,
+      label: report.label,
+      official: report.official,
+      failed: !!report.analysis.find(cur => cur.error),
       coverage: {
         total,
         covered,
@@ -106,7 +118,7 @@ function generateStats(fullReport) {
 function analyze() {
   const report = {
     date: new Date(),
-    title: "NPC Advenures localization coverage report",
+    title: "NPC Adventures localization coverage report",
     reports: [],
     stats: null,
   };
@@ -114,7 +126,12 @@ function analyze() {
   console.log("Analyzing assets...");
 
   for (let locale of supportedLocales) {
-    report.reports.push({locale, analysis: walk(locale, knownContents)});
+    report.reports.push({
+      locale: locale.code, 
+      label: locale.name, 
+      official: locale.official, 
+      analysis: walk(locale, knownContents)
+    });
   }
 
   console.log("Generating stats...");
@@ -123,12 +140,28 @@ function analyze() {
   return report;
 }
 
+function mark(stat) {
+  if (stat.failed) {
+    return "F";
+  }
+
+  if (stat.coverage.percentage <= 0.5) {
+    return "!"
+  }
+
+  if (stat.coverage.percentage < 0.8) {
+    return "*"
+  }
+
+  return " ";
+}
+
 const analysis = analyze();
 
 for (let stat of analysis.stats) {
-  console.log(`Locale: ${stat.locale} - Covered ${stat.coverage.covered} entries of ${stat.coverage.total} (${Number(stat.coverage.percentage * 100).toFixed(2)}%)`);
+  console.log(` ${mark(stat)} Locale: ${stat.label} (${stat.locale}) - Covered ${stat.coverage.covered} entries of ${stat.coverage.total} (${Number(stat.coverage.percentage * 100).toFixed(2)}%)`);
 }
 
-require("fs").writeFileSync("report.json", JSON.stringify(analysis));
-console.log("Report written to report.json");
-
+const reportFile = process.argv[2] || "report.json";
+require("fs").writeFileSync(reportFile, JSON.stringify(analysis));
+console.log(`Report written to ${reportFile}`);
